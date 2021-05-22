50f9f6b5ca3b143abda82e25c164fcc9b4ab4dae
Ivan Sandratskiy <ivan.sandratskiy-ext@ledger.fr>
2021-05-22 05:21:58 +0300

    v1.0.8

--- src/setup.ts
+++ src/setup.ts
@@ -1,32 +1,30 @@
-import { execSync } from "child_process";
 import * as core from "@actions/core";
-
-console.log(`INSTALLING LEDGER VAULT CLI`);
+import * as exec from "@actions/exec";
 
 const PACKAGE_MANAGER = core.getInput("package-manager");
 const VAULT_CLI_VERSION = core.getInput("version");
 
 async function run() {
+  console.log(`INSTALLING LEDGER VAULT CLI`);
+
   try {
     switch (PACKAGE_MANAGER) {
       case "npm":
-        execSync(
-          `sudo npm install -g @ledgerhq/vault-cli@${VAULT_CLI_VERSION} --loglevel=error`,
-          { stdio: "inherit" }
+        await exec.exec(
+          `sudo npm install -g @ledgerhq/vault-cli@${VAULT_CLI_VERSION} --loglevel=error`
         );
         break;
       case "yarn":
-        execSync(
-          `sudo yarn global add @ledgerhq/vault-cli@${VAULT_CLI_VERSION}`,
-          { stdio: "inherit" }
+        await exec.exec(
+          `sudo yarn global add @ledgerhq/vault-cli@${VAULT_CLI_VERSION}`
         );
         break;
     }
   } catch (err) {
     core.setFailed(err.message);
   }
+
+  console.log(`LEDGER VAULT CLI INSTALLED SUCCESSFULLY`);
 }
 
 run();
-
-console.log(`LEDGER VAULT CLI INSTALLED SUCCESSFULLY`);
