eb6b9763cadc2553a78036133916bc8233a12f61
Ivan Sandratskiy <ivan.sandratskiy-ext@ledger.fr>
2021-06-15 03:28:31 +0300

    move to yarn, yarn as default now, clean up

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -1,24 +1,24 @@
-import { exec } from "@actions/exec";
-import * as core from "@actions/core";
+import { exec } from '@actions/exec'
+import { getBooleanInput, getInput, setFailed } from '@actions/core'
 
-const PACKAGE_MANAGER = core.getInput("package_manager");
-const SUDO = core.getBooleanInput("with_sudo") ? "sudo" : "";
+const PACKAGE_MANAGER = getInput('package_manager')
+const SUDO = getBooleanInput('with_sudo') ? 'sudo' : ''
 
 async function run() {
-  console.log(`REMOVE LEDGER VAULT CLI`);
-
-  try {
-    switch (PACKAGE_MANAGER) {
-      case "npm":
-        await exec(`${SUDO} npm uninstall -g @ledgerhq/vault-cli`);
-        break;
-      case "yarn":
-        await exec(`${SUDO} yarn global remove @ledgerhq/vault-cli`);
-        break;
-    }
-  } catch (err) {
-    core.setFailed(err.message);
-  }
+	try {
+		switch (PACKAGE_MANAGER) {
+			case 'npm':
+				console.log('REMOVE LEDGER VAULT CLI VIA NPM')
+				await exec(`${SUDO} npm uninstall -g @ledgerhq/vault-cli`)
+				break
+			case 'yarn':
+				console.log('REMOVE LEDGER VAULT CLI VIA YARN')
+				await exec(`${SUDO} yarn global remove @ledgerhq/vault-cli`)
+				break
+		}
+	} catch (err) {
+		setFailed(err.message)
+	}
 }
 
-run();
+run()
--- src/setup.ts
+++ src/setup.ts
@@ -1,29 +1,27 @@
-import * as core from "@actions/core";
-import { exec } from "@actions/exec";
+import { exec } from '@actions/exec'
+import { getBooleanInput, getInput, setFailed } from '@actions/core'
 
-const PACKAGE_MANAGER = core.getInput("package_manager");
-const VAULT_CLI_VERSION = core.getInput("vault_cli_version");
-const SUDO = core.getBooleanInput("with_sudo") ? "sudo" : "";
+const PACKAGE_MANAGER = getInput('package_manager')
+const VAULT_CLI_VERSION = getInput('vault_cli_version')
+const SUDO = getBooleanInput('with_sudo') ? 'sudo' : ''
 
 async function run() {
-  console.log(`INSTALLING LEDGER VAULT CLI`);
-
-  try {
-    switch (PACKAGE_MANAGER) {
-      case "npm":
-        await exec(
-          `${SUDO} npm install --unsafe-perm -g @ledgerhq/vault-cli@${VAULT_CLI_VERSION} --loglevel=error`
-        );
-        break;
-      case "yarn":
-        await exec(
-          `${SUDO} yarn global add @ledgerhq/vault-cli@${VAULT_CLI_VERSION}`
-        );
-        break;
-    }
-  } catch (err) {
-    core.setFailed(err.message);
-  }
+	try {
+		switch (PACKAGE_MANAGER) {
+			case 'npm':
+				console.info('INSTALLING LEDGER VAULT CLI VIA NPM')
+				await exec(
+					`${SUDO} npm install --unsafe-perm -g @ledgerhq/vault-cli@${VAULT_CLI_VERSION} --loglevel=error`
+				)
+				break
+			case 'yarn':
+				console.info('INSTALLING LEDGER VAULT CLI VIA YARN')
+				await exec(`${SUDO} yarn global add @ledgerhq/vault-cli@${VAULT_CLI_VERSION}`)
+				break
+		}
+	} catch (err) {
+		setFailed(err.message)
+	}
 }
 
-run();
+run()
